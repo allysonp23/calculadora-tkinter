af3f206f887c487304f4fa10c3a1e48ec84b0503
Allyson <allysonpaulino23@gmail.com>
2023-05-02 10:31:29 -0300

    Calculadora 1.1.0

--- calculator_class.py
+++ calculator_class.py
@@ -1,3 +1,5 @@
+import re
+import math
 import tkinter as tk
 from typing import List
 
@@ -33,16 +35,62 @@ class Calculator:
         for row_values in buttons:
             for button in row_values:
                 button_text = button['text']
+                # Botão C limpa a Label
                 
                 if button_text == 'C':
                     button.bind('<Button-1>', self.clear)
+                
+                # Entrada dos demais botões
+                if button_text in '0123456789.+-/*()^':
+                    button.bind('<Button-1>', self.add_text_to_display)
+                
+                # Entrada do botão '=' para realizar a operação
+                if button_text == '=':
+                    button.bind('<Button-1>', self.calculate)
    
-
-    
+    # Binds para: Tecla enter realiza o cálculo e 
     def _config_display(self):
-        ...
+        self.display.bind('<Return>', self.calculate)
+        self.display.bind('<KP_Enter>', self.calculate)
+
+    def _fix_text(self, text):
+        # Substitui tudo que não for: 0123456789./*-+^ 
+        text = re.sub(r'[^\d\.\/\*\-\+\^\(\)e]', r'', text, 0)
+        # Substitui operadores repetidos
+        text = re.sub(r'([\.\+\/\-\*\^])\1+',r'\1', text, 0)
+        # Substitui () ou *() para 'nada'
+        text = re.sub(r'\*?\(\)','', text)
+        
+        return text
+        
         
     def clear (self, event=None):
         self.display.delete(0, 'end')
         
+    def add_text_to_display(self, event=None):
+        self.display.insert('end', event.widget['text'])
+    
+    def calculate(self, event=None):
+        fixed_text = self._fix_text(self.display.get())
+        equations = self._get_equations(fixed_text)
         
+        try:
+            if len(equations) == 1:
+                result = eval(self._fix_text(equations[0]))
+            else: 
+                result = eval(self._fix_text(equations[0]))
+                for equation in equations [1:]:
+                    result = math.pow(result, eval(self._fix_text(equation)))
+        
+            self.display.delete(0, 'end')
+            self.display.insert('end', result)
+            self.label.config(text=f'{fixed_text} = {result}')
+        
+        except OverflowError:
+            self.label.config(text= "Não consegui realizar essa conta.")
+        except Exception as e:
+            self.label.config(text='Conta inválida.')
+            print(e)
+            
+    def _get_equations(self, text):
+        return re.split(r'\^', text, 0)
